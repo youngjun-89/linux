7f2bb7e959c3a7ea3e15ccff01aa87391357dc4c
YoungJun.park <her0gyugyu@gmail.com>
2022-09-27 01:37:11 -0700

    task_struct에 stack_end 값 추가

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1512,6 +1512,9 @@ struct task_struct {
 	union rv_task_monitor		rv[RV_PER_TASK_MONITORS];
 #endif
 
+#ifdef CONFIG_STACK_CTX
+	void *stack_ctx_end;
+#endif
 	/*
 	 * New fields for task_struct should be added above here, so that
 	 * they are included in the randomized portion of task_struct.
